2a08335ee1f424c7c1533b3d898d5d0654976005
danille <danilleock@gmail.com>
2017-07-29 23:07:37 +0200

    Front page is now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -34,7 +34,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+                        'New to-do item did not appear in table')
         # There is still textbox inviting her to add another to-do item.
         # She enters "Use peacock feathers to make a fly" and hits Enter.
         self.fail('Finish the test!')
